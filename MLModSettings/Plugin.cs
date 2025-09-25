bcfb244ba1b6190ad41b5b3e52f9320229b1e02e
NepuShiro <nepushiro@nepunep.xyz>
2025-09-24 22:22:20 -0500

    Add GNU General Public License (GPLv3) to the project and remove placeholder files

--- MLModSettings/Plugin.cs
+++ MLModSettings/Plugin.cs
@@ -31,21 +31,36 @@ public class Plugin : BasePlugin
 
             HarmonyInstance.Patch(
                 AccessTools.Method(typeof(FeedItemInterface), nameof(FeedItemInterface.Set), new Type[] { typeof(IDataFeedView), typeof(DataFeedItem) }),
-                postfix: new HarmonyMethod(typeof(PatchClass), nameof(PatchClass.NoMoreMlSettings))
+                postfix: new HarmonyMethod(typeof(PatchClass), nameof(PatchClass.HideMlSettingsCategory))
             );
 
             HarmonyInstance.Patch(
                 AccessTools.Method(typeof(MonkeyLoaderRootCategorySettingsItems), nameof(MonkeyLoaderRootCategorySettingsItems.Apply)),
                 prefix: new HarmonyMethod(typeof(PatchClass), nameof(PatchClass.LogMlPath))
             );
+            
+            Log.LogInfo($"Plugin {PluginMetadata.GUID} is fully loaded!");
         };
 
-        Log.LogInfo($"Plugin {PluginMetadata.GUID} is loaded!");
+        Log.LogInfo($"Plugin {PluginMetadata.GUID} is partially loaded!");
     }
 
     private class PatchClass
     {
-        public static void NoMoreMlSettings(FeedItemInterface __instance)
+        public static async IAsyncEnumerable<DataFeedItem> CreateMlCategory(IReadOnlyList<string> path)
+        {
+            await Task.CompletedTask;
+
+            DataFeedGroup pluginsGroup = new DataFeedGroup();
+            pluginsGroup.InitBase("MLSettingsGroup", path, null!, "Settings.Category.MonkeyLoader".AsLocaleKey());
+            yield return pluginsGroup;
+
+            DataFeedCategory mlSettingsCategory = new DataFeedCategory();
+            mlSettingsCategory.InitBase("MonkeyLoaderMLSettings", path, ["MLSettingsGroup"], "MonkeyLoader.GamePacks.Resonite.OpenMonkeyLoader.Name".AsLocaleKey(), "MonkeyLoader.GamePacks.Resonite.OpenMonkeyLoader.Description".AsLocaleKey());
+            yield return mlSettingsCategory;
+        }
+
+        public static void HideMlSettingsCategory(FeedItemInterface __instance)
         {
             SetDataFeedCategory setDfc = __instance.Slot.GetComponent<SetDataFeedCategory>();
             if (setDfc == null) return;
@@ -61,19 +76,6 @@ public class Plugin : BasePlugin
             {
                 Log.LogDebug($"Current Path: {string.Join(" -> ", parameters.Path)}");
             }
-        }
-
-        public static async IAsyncEnumerable<DataFeedItem> CreateMlCategory(IReadOnlyList<string> path)
-        {
-            await Task.CompletedTask;
-
-            DataFeedGroup pluginsGroup = new DataFeedGroup();
-            pluginsGroup.InitBase("MLSettingsGroup", path, null!, "Settings.Category.MonkeyLoader".AsLocaleKey());
-            yield return pluginsGroup;
-
-            DataFeedCategory mlSettingsCategory = new DataFeedCategory();
-            mlSettingsCategory.InitBase("MonkeyLoaderMLSettings", path, ["MLSettingsGroup"], "MonkeyLoader.GamePacks.Resonite.OpenMonkeyLoader.Name".AsLocaleKey(), "MonkeyLoader.GamePacks.Resonite.OpenMonkeyLoader.Description".AsLocaleKey());
-            yield return mlSettingsCategory;
         }
 
         public static async IAsyncEnumerable<DataFeedItem> GotoMonkeyLoader(IReadOnlyList<string> path)
